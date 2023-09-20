33bc3b9f8ea4cc0cea56e491cb5cf3c1dc53d6d9
ben <bentan1020@gmail.com>
2023-09-20 15:55:48 -0400

    set up routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,23 @@
+import Public from "./pages/Public/Public";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  useNavigate,
+} from "react-router-dom";
+
+import Home from "./pages/Home/Home";
+import Login from "./pages/Login/Login";
+
 function App() {
   return (
-    <div className="h-screen bg-slate-500">
-      <div className="">Hello World</div>;
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Public />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 
