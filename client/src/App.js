506d71dd857aeadd72095997def99ebbfcc645a3
ben <bentan1020@gmail.com>
2023-09-20 12:47:03 -0400

    boilerplate for react

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,7 @@
-import './index.css';
+import "./index.css";
 
 function App() {
-  return (
-    <div className="bg-slate-500">
-    Hello World
-    </div>
-  );
+  return <div className="bg-slate-500">Hello World</div>;
 }
 
 export default App;
