aa8b1ae692081e87701e9fad8d35d61d296fff13
donutballer10 <47674686+donutballer10@users.noreply.github.com>
2023-09-21 01:10:06 -0400

    last commit didnt load register, hoepfully this one does

--- client/src/App.js
+++ client/src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Route path="/" element={<Public />} />
         <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
