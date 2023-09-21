3fbde65f71fdf92e80d8738eba5ce986b6413331
Erik Lichtenstein <eriklichtenstein2@gmail.com>
2023-09-21 01:58:58 -0400

    after a million 'Adjacent JSX elements must be wrapped in an enclosing tag' elements finally got page to load

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -3,8 +3,20 @@ import './Register.css'
 
 const Register = () => {
   return (
+		<div>
     <h1 className="logo-purple"> Oauthpal </h1>
+		</div>
   )
+}
+
+const Form = () => {
+	return (
+		<form>
+			<label> First Name
+				<input type="text" />
+			</label>
+		</form>
+	)
 }
 
 export default Register 
