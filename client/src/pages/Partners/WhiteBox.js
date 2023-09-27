fb8398d28478c0d49af1f9eb0bfd1579688d3dcc
+@??@$ <62633243+ham-mad@users.noreply.github.com>
2023-09-27 15:39:15 -0400

    Added Both Messaging pages

---
+++ client/src/pages/Partners/WhiteBox.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const WhiteBox = () => {
+  return (
+    <svg
+      
+      width={331}
+      height={982}
+      
+      fill="none"
+      
+    >
+      <path d="M0 0 H331 V982 H0 V0 Z" fill="white" />
+    </svg>
+  );
+};
+
+export default WhiteBox;
