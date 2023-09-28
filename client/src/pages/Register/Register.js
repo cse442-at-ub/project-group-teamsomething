4fc1cfbb8ef9a39e2c5cde12d531ef102ca8ae3c
Erik Lichtenstein <eriklichtenstein2@gmail.com>
2023-09-28 12:55:53 -0400

    added reg button. went ok. then login button and screwed thigns up. still not sure how to size micro elements

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -6,18 +6,33 @@ const Register = () => {
   return (
 		<div className='bg'> 
 			<h1 className='logo'> Oauthpal </h1>
+
 			<h2 className="form-title row-start-2 col-start-2">Register</h2>
 			<form className="form-box">
+
+				<div className="fname">
 				<label className="label">First Name</label>
       			<input className="text-box" type="text" />
-				<label className="label"> Last Name</label>
+				</div>
+
+				<div className="lname">
+				<label className="label">Last Name</label>
       			<input className="text-box" type="text" />
-				<br/>
-      			<label className="label">Email</label>
+				</div>
+
+				<div className="email">
+				<label className="label">Email</label>
       			<input className="text-box" type="text" />
-				<br/>
-     		 	<label className="label">Password</label>
+				</div>
+
+				<div className="pw">
+				<label className="label">Password</label>
       			<input className="text-box" type="text" />
+				</div>
+
+				<button className="reg-button">Register</button>
+				<button className="login-button">Login</button>
+
 			</form>
 		</div>
   )
