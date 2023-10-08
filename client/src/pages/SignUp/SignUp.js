2d0a7391aea2c358c0136f61bab2fd8f1a578304
ben <bentan1020@gmail.com>
2023-10-08 13:13:32 -0400

    Revert "Current backend server is working"
    This reverts commit 216feb1330997efd1767e86938b5e74194669261.

--- client/src/pages/SignUp/SignUp.js
+++ client/src/pages/SignUp/SignUp.js
@@ -1,45 +1,9 @@
-import React from "react";
+import React from 'react'
 
-import background from "../../assets/loginBackground.png";
-import google from "../../assets/googleIcon.png";
-import facebook from "../../assets/facebookIcon.png";
-
-import { useNavigate } from "react-router-dom";
 const SignUp = () => {
-  const navigate = useNavigate();
-
   return (
-    <div className="h-screen bg-[#FED99B] relative flex items-center justify-center">
-      <div className="w-[500px] h-auto bg-[#662c91] rounded-3xl flex flex-col pt-8 pb-8 items-center z-10">
-        <form className="w-11/12">
-          <div className="mb-5">
-            <p className="text-white">Email</p>
-            <input className="bg-[#FFFFFF] w-full h-8 rounded-3xl mb-4"></input>
+    <div>SignUp</div>
+  )
+}
 
-            <p className="text-white">Password</p>
-            <input className="bg-[#FFFFFF] w-full h-8 rounded-3xl"></input>
-          </div>
-
-          <div className="flex flex-row gap-4">
-            <button className="bg-[#FF3737] rounded-3xl px-5 py-2 text-white">
-              SignUp
-            </button>
-            <button
-              onClick={() => {
-                navigate("/sign-up");
-              }}
-              className="text-white"
-            >
-              Register
-            </button>
-          </div>
-        </form>
-      </div>
-      <div className="absolute bottom-0 right-0 z-0 hidden md:block">
-        <img src={background} alt="Background Image"></img>
-      </div>
-    </div>
-  );
-};
-
-export default SignUp;
+export default SignUp
