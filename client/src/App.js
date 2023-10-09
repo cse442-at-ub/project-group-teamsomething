cd947935599a24f40f0c4a037a530bc9777478de
ben <bentan1020@gmail.com>
2023-10-09 11:42:42 -0400

    remove unncessary pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,33 @@
-import Public from "./pages/public/Public";
 import {
-  BrowserRouter as Router,
   Route,
+  BrowserRouter as Router,
   Routes,
 } from "react-router-dom";
+import Public from "./pages/public/Public";
 
+import CheckIn from "./pages/CheckIn/CheckIn";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
-import CheckIn from "./pages/CheckIn/CheckIn";
+import Messages from "./pages/Messages/Messages";
+import Partners from "./pages/Partners/Partners";
 import ChangePassword from "./pages/Profile/ChangePassword";
 import ProfilePic from "./pages/Profile/ProfilePic";
-import Messages from "./pages/Messages/Messages"
-import Partners from "./pages/Partners/Partners"
 import SignUp from "./pages/SignUp/SignUp";
-import Register from "./pages/Register/Register";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Public />} />
-        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/home" element={<Home />} />
+        
         <Route path="/check-in" element={<CheckIn />} />
         <Route path="/changepassword" element={<ChangePassword />} />
         <Route path="/profilepic" element={<ProfilePic />} />
         <Route path="/Messages" element={<Messages />} />
         <Route path="/Partners" element={<Partners />} />
-        <Route path="/register" element={<Register />} />
       </Routes>
     </Router>
   );
