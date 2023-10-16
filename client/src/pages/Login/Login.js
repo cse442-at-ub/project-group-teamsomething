3e1fd8ee09c8a701cffa4ed374c7056ef6de39f6
IanHunter <31974794+Ian-hunter1@users.noreply.github.com>
2023-10-16 13:48:43 -0400

    fully completed register and login using txt file db

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,13 +1,30 @@
 import React from "react";
+import axios from "axios";
+import { useState } from "react";
 
 import background from "../../assets/loginBackground.png";
 import google from "../../assets/googleIcon.png";
 import facebook from "../../assets/facebookIcon.png";
 
 import { useNavigate } from "react-router-dom";
+
 const Login = () => {
   const navigate = useNavigate();
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
+  const loginHandler = async (event) => {
+    console.log(username);
+    console.log(password);
+    event.preventDefault();
+    try {
+      const res = await axios.post("http://localhost:8000/login", { username, password });
+      console.log(res);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  
   return (
     <div className="h-screen bg-[#FED99B] relative flex items-center justify-center">
       <div className="w-[500px] h-auto bg-[#662c91] rounded-3xl flex flex-col pt-8 pb-8 items-center z-10">
@@ -15,24 +32,37 @@ const Login = () => {
           <img src={google} width={30} height={30} alt="Google Logo" />
           <p className="text-xl">Log in with Google</p>
         </button>
+
         <button className="bg-white w-11/12 h-12 mb-6 flex flex-row justify-center items-center gap-5 rounded-xl">
           <img src={facebook} width={30} height={30} alt="Facebook Logo" />{" "}
           <p className="text-xl">Log in with Facebook</p>
         </button>
-        <form className="w-11/12">
+
+        <form onSubmit={loginHandler} className="w-11/12">
           <div className="mb-5">
-            <p className="text-white">Email</p>
-            <input className="bg-[#FFFFFF] w-full h-8 rounded-3xl mb-4"></input>
+            <p className="text-white">Username</p>
+            <input 
+              type="text" 
+              className="bg-[#FFFFFF] w-full h-8 rounded-3xl mb-4" 
+              value={username} 
+              onChange={(e) => setUsername(e.target.value)}
+            />
 
             <p className="text-white">Password</p>
-            <input className="bg-[#FFFFFF] w-full h-8 rounded-3xl"></input>
+            <input 
+              type="password" 
+              className="bg-[#FFFFFF] w-full h-8 rounded-3xl" 
+              value={password} 
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
 
           <div className="flex flex-row gap-4">
-            <button className="bg-[#FF3737] rounded-3xl px-5 py-2 text-white">
+            <button type="submit" className="bg-[#FF3737] rounded-3xl px-5 py-2 text-white">
               Login
             </button>
             <button
+              type="button"
               onClick={() => {
                 navigate("/sign-up");
               }}
--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,16 +1,35 @@
 import React from 'react'
 import './Register.css'
+import axios from "axios";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 var cheshire= "https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-442x/Register.php"
 // var local = "http://localhost:80/442/Register.php"
+  
 
 const Register = () => {
+	const navigate = useNavigate();
+	const [username, setUsername] = useState("");
+	const [password, setPassword] = useState("");
+  
+	const registerHandler = async (event) => {
+	  console.log(username);
+	  console.log(password);
+	  event.preventDefault();
+	  try {
+		const res = await axios.post("http://localhost:8000/register", { username, password });
+		console.log(res);
+	  } catch (error) {
+		console.error(error);
+	  }
+	};
   return (
 		<div className='bg'>
 
 			<h1 className='logo'> Oauthpal </h1>
 			<h2 className="form-title row-start-2 col-start-2">Register</h2>
-			<form action={cheshire}  method="post" className="form-box">
+			<form action={registerHandler}  method="post" className="form-box">
 
 				<div className="fname">
 				<label className="label">First Name</label>
@@ -22,14 +41,20 @@ const Register = () => {
 				<input className="text-box" type="text" />
 				</div>
 
-				<div className="email">
-				<label className="label">Email</label>
-				<input className="text-box" type="text" />
+				<div className="username">
+				<label className="label">Username</label>
+				<input className="text-box" type="text" 
+				value={username}
+				onChange={(e) => setUsername(e.target.value)}
+				/>
 				</div>
 
 				<div className="pw">
 				<label className="label">Password</label>
-				<input className="text-box" type="text" />
+				<input className="text-box" type="text" 
+				value={password}
+				onChange={(e) => setPassword(e.target.value)}
+				/>
 				</div>
 
 				<button type="submit" method="post" className="reg-button">Register</button>
@@ -42,14 +67,5 @@ const Register = () => {
 		</div>
   )
 }
-
-/*
-const Form = () => {
-	return (
-		<form className="form-box">
-		</form>
-	)
-}
-*/
 
 export default Register 
