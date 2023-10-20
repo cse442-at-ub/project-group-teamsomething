38b50e03ba272641d6c2eb1c662297bfb8d261ea
donutballer10 <47674686+donutballer10@users.noreply.github.com>
2023-10-20 13:17:19 -0400

    add cheshire routing back

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,18 +14,23 @@ import Matches from "./pages/Matches/Matches";
 import Message from "./pages/Message/Message";
 import Profile from "./pages/Profile/Profile";
 
+var cheshire = "/CSE442-542/2023-Fall/cse-442x/build";
+//var cheshire = ''
+
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Public />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/message" element={<Message />} />
-        <Route path="/partners"element={<Partners />} />
-        <Route path="/matches" element={<Matches />} />
-        <Route path="/profile" element={<Profile />} />        
+        <Route path= {cheshire + "/"} element={<Public />} />
+        <Route path= {cheshire + "/home"} element={<Home />} />
+        <Route path= {cheshire + "/login"} element={<Login />} />
+        <Route path= {cheshire + "/check-in"} element={<CheckIn />} />
+        <Route path= {cheshire + "/changepassword"} element={<ChangePassword />} />
+        <Route path= {cheshire + "/profilepic"} element={<ProfilePic />} />
+        <Route path= {cheshire + "/message"} element={<Message />} />
+        <Route path= {cheshire + "/partners"} element={<Partners />} />
+        <Route path= {cheshire + "/sign-up"} element={<SignUp />} />
+        <Route path= {cheshire + "/matches"} element={<Matches />} />    
       </Routes>
     </Router>
   );
