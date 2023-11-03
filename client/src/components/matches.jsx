2c1eecfa5ead951c158ec3a24043915ae05eeb7e
Erik Lichtenstein <eriklichtenstein2@gmail.com>
2023-11-02 21:54:04 -0400

    ok so onload functionality works basically. will check if user has partner already and block if do. and then requesting someone who has partner, rquesting someone who doesnt, they both work. just need to add code from load situation back to button click situation so user cant send fr when free then another after having already sent one

--- client/src/components/matches.jsx
+++ client/src/components/matches.jsx
@@ -5,20 +5,21 @@ var cheshire = "https://www-student.cse.buffalo.edu/CSE442-542/2023-Fall/cse-442
 
 const Matches = () => {
 
+    const [state, setState] = useState({matches:[]});
+    const [userTaken, setStatus] = useState(false);
+
     const matchAccepted = (match, event) => {
-		event.preventDefault();
         const user = document.getElementById("username").textContent;
+		event.preventDefault();
 		axios.post(cheshire, {
 				accepter: user,
-				accepted: match
+				accepted: match,
+                action: "accept"
 			})
 			.then((response) => {
 				console.log(response);
                 if (response['data'] == "fr accepted"){
                     alert("friend request sent!");
-                }
-                else if (response['data'] == "user has partner"){
-                    document.getElementById("yourMatches").innerHTML = "user already has partner, go to matches page to talk with them";
                 }
                 else if (response['data'] == "partner taken"){
                     alert("match has already found a partner, please reload page to see current matches");
@@ -29,19 +30,30 @@ const Matches = () => {
 			});
 	};
 
-    const [state, setState] = useState({matches:[]})
-
     useEffect(() => {
-        axios.get(cheshire)
+        const user = document.getElementById("username").textContent;
+        axios.post(cheshire, {accepter: user, action: "load"})
         .then((response) => {
-            setState({...state,matches:[response['data']]});
             console.log(response['data']);
+            if (response['data'] == "user has partner"){
+                console.log("steph");
+                setStatus(true);
+            }
+            else {
+                setState({...state,matches:[response['data']]});
+            }
         })
         .catch((error) => {
             console.log(error);
         });
     },[])
 
+    console.log(userTaken);
+    if (userTaken) { 
+        console.log("lebron");
+        //document.getElementById("yourMatches").innerHTML = "user already has partner, go to matches page to talk with them";
+        return <div> user already has partner, go to matches page to talk with them </div>
+    }
 
     const x = state['matches'][0]?.map((match) => 
         <div>
@@ -52,7 +64,8 @@ const Matches = () => {
         </div>
     );
 
-    return <div>{x}</div>
+    return <div>{x}</div> 
+
 }
 
 export default Matches
--- server/matches.php
+++ server/matches.php
@@ -4,34 +4,22 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
-if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    // Connect to db
-    $conn = new mysqli("oceanus.cse.buffalo.edu", "eriklich", "teamsomething", "cse442_2023_fall_team_x_db");
-    if ($conn->connect_error) {
-        http_response_code(500);
-        echo json_encode(['message' => "Connection failed: " . $conn->connect_error]);
-		}
-		$usernames = $conn->query("SELECT username FROM users")->fetch_all();
-		echo json_encode($usernames);
-		exit();
-}
-
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $data = json_decode(file_get_contents("php://input"), true);
-    $p1 = $data['accepter'];
-    $p2 = $data['accepted'];
+  $data = json_decode(file_get_contents("php://input"), true);
+	$action = $data['action'];
+	$p1 = $data['accepter'];
 
-		echo $accepter;
-		echo $accepted;
 
-    // Connect to db
-    $conn = new mysqli("oceanus.cse.buffalo.edu", "eriklich", "teamsomething", "cse442_2023_fall_team_x_db");
-    if ($conn->connect_error) {
-        http_response_code(500);
-        echo json_encode(['message' => "Connection failed: " . $conn->connect_error]);
-		}
+	// Connect to db
+	$conn = new mysqli("oceanus.cse.buffalo.edu", "eriklich", "teamsomething", "cse442_2023_fall_team_x_db");
+	if ($conn->connect_error) {
+		http_response_code(500);
+		echo json_encode(['message' => "Connection failed: " . $conn->connect_error]);
+	}
 
+	if ($action == "load"){
+		
 		// check if user has partner already, if so page should be blocked
 		$sql = "SELECT partner FROM users WHERE username=?";
 		$stmt = $conn->prepare($sql);
@@ -44,48 +32,60 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 			echo "user has partner";
 			exit();
 		}
+		$usernames = $conn->query("SELECT username FROM users")->fetch_all();
+		echo json_encode($usernames);
+		exit();
+	}
 
-		// check if partner got accepted by someone else already, if so send failure message back
-		$sql = "SELECT partner FROM users WHERE username=?";
-		$stmt = $conn->prepare($sql);
-		$stmt->bind_param("s", $p2);
-		$stmt->execute();
-		$result = $stmt->get_result();
-		$partner = $result->fetch_assoc()["partner"];
-		// echo $partner . " is partner";
-		if (!is_null($partner)) {
-			echo "partner taken";
+	else {
+		$p2 = $data['accepted'];
+
+			echo $accepter;
+			echo $accepted;
+
+			// check if partner got accepted by someone else already, if so send failure message back
+			$sql = "SELECT partner FROM users WHERE username=?";
+			$stmt = $conn->prepare($sql);
+			$stmt->bind_param("s", $p2);
+			$stmt->execute();
+			$result = $stmt->get_result();
+			$partner = $result->fetch_assoc()["partner"];
+			// echo $partner . " is partner";
+			if (!is_null($partner)) {
+				echo "partner taken";
+				exit();
+			}
+
+			$stmt = $conn->prepare("UPDATE users SET partner=? WHERE username=?");
+			if (!$stmt) {
+			http_response_code(500);
+			echo json_encode(['message' => "Statement preparation failed: " . $conn->error]);
+			exit();
+			}
+			// set accepter's partner
+			$stmt->bind_param("ss", $p1, $p2);
+			if (!$stmt->execute()) {
+			http_response_code(500);
+			echo json_encode(['message' => "Statement execution failed: " . $stmt->error]);
 			exit();
 		}
 
-		$stmt = $conn->prepare("UPDATE users SET partner=? WHERE username=?");
-		if (!$stmt) {
-        http_response_code(500);
-        echo json_encode(['message' => "Statement preparation failed: " . $conn->error]);
-        exit();
+			// set accepted's partner 
+			$p1 = $data['accepted']; 
+			$p2 = $data['accepter'];
+			if (!$stmt->execute()) {
+			http_response_code(500);
+			echo json_encode(['message' => "Statement execution failed: " . $stmt->error]);
+			exit();
 		}
-		// set accepter's partner
-		$stmt->bind_param("ss", $p1, $p2);
-		if (!$stmt->execute()) {
-        http_response_code(500);
-        echo json_encode(['message' => "Statement execution failed: " . $stmt->error]);
-        exit();
+
+			$stmt->close();
+			$conn->close();
+
+			echo "fr accepted";
+		exit();
     }
 
-		// set accepted's partner 
-		$p1 = $data['accepted']; 
-		$p2 = $data['accepter'];
-		if (!$stmt->execute()) {
-        http_response_code(500);
-        echo json_encode(['message' => "Statement execution failed: " . $stmt->error]);
-        exit();
-    }
-
-		$stmt->close();
-		$conn->close();
-
-		echo "fr accepted";
-    exit();
-    }
+}
 
 ?>
