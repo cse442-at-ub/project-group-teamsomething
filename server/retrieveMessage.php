0ae350aac744435fd5b1cfdfb98ce60c8848d3bc
IanHunter <31974794+Ian-hunter1@users.noreply.github.com>
2023-11-09 14:51:14 -0500

    added logic for storing messages untested

--- server/retrieveMessage.php
+++ server/retrieveMessage.php
@@ -10,6 +10,25 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+// Function to establish a database connection
+function connectToDatabase()
+{
+    $host = 'oceanus.cse.buffalo.edu';
+    $username = 'ianhunte';
+    $password = '50406690';
+    $database = 'cse442_2023_fall_team_x_db';
+
+    $conn = new mysqli($host, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        http_response_code(500);
+        echo json_encode(['message' => "Connection failed: " . $conn->connect_error]);
+        exit();
+    }
+
+    return $conn;
+}
+
 // Retrieve all the message history for a given user
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $data = json_decode(file_get_contents("php://input"), true);
@@ -17,24 +36,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $sender_username = $data['sender_username'] ?? '';
     $receiver_username = $data['receiver_username'] ?? '';
 
-    // Connect to db
-    $conn = new mysqli("oceanus.cse.buffalo.edu", "username", "password", "cse442_2023_fall_team_x_db");
-    if ($conn->connect_error) {
-        http_response_code(500);
-        echo json_encode(['message' => "Connection failed: " . $conn->connect_error]);
-        exit();
-    }
+    // Connect to the database
+    $conn = connectToDatabase();
 
-    // Assuming 'username' is unique for each user
-    $stmt = $conn->prepare("SELECT * FROM messages WHERE sender_username=? OR receiver_username=? ORDER BY timestamp ASC");
-    $stmt->bind_param("ss", $sender_username, $receiver_username);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    // Insert the new message into the messages table
+    $stmtInsert = $conn->prepare("INSERT INTO messages (sender_username, receiver_username, content, timestamp) VALUES (?, ?, ?, ?)");
+    $currTime = time();
+    $stmtInsert->bind_param("ssss", $sender_username, $receiver_username, $text, $currTime);
+    $stmtInsert->execute();
+    $stmtInsert->close();
+
+    // Fetch the updated message history
+    $stmtSelect = $conn->prepare("SELECT * FROM messages WHERE sender_username=? OR receiver_username=? ORDER BY timestamp ASC");
+    $stmtSelect->bind_param("ss", $sender_username, $receiver_username);
+    $stmtSelect->execute();
+    $result = $stmtSelect->get_result();
     $messages = [];
     while ($row = $result->fetch_assoc()) {
         $messages[] = $row;
     }
-    $stmt->close();
+    $stmtSelect->close();
     $conn->close();
 
     echo json_encode($messages);
