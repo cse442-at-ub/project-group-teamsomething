ea1c4f11c9e8ab744d9f9b9c0c402b29e6cc019e
keejoonam <60615482+keejoonam@users.noreply.github.com>
2023-11-11 00:47:54 -0500

    recover missing code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,6 +15,7 @@ import PastContract from "./pages/User/PastContract/PastContract.jsx";
 import PartnerReview from "./pages/User/PartnerReview/PartnerReview.jsx";
 import Payment from "./pages/User/Payment/Payment.jsx";
 import ChangePassword from "./pages/User/ChangePassword/ChangePassword";
+import Message from "./pages/Message/Message.jsx";
 
 import { useAuth } from "./hooks/auth-hook";
 import { AuthContext } from "./context/auth-context";
@@ -83,7 +84,7 @@ function App() {
     },
   });
 
-  const { username, login, logout } = useAuth();
+  const { username, fname, lname, partner, login, logout } = useAuth();
   let protectedRoutes;
   if (username) {
     protectedRoutes = (
@@ -97,6 +98,7 @@ function App() {
         <Route path={"/change-password"} element={<ChangePassword />} />
         <Route path={"/partner-reviews"} element={<PartnerReview />} />
         <Route path={"/payment"} element={<Payment />} />
+        <Route path={"/message"} element={<Message />} />
         <Route path={"/message"} element={<Message_Blocked/>} />
         <Route path={"/partners"} element={<Partners />} />
         <Route path={"/matches"} element={<Matches />} />
@@ -111,6 +113,9 @@ function App() {
           username: username,
           login: login,
           logout: logout,
+          fname: fname,
+          lname: lname,
+          partner: partner
         }}
       >
         <Router basename="/CSE442-542/2023-Fall/cse-442x/dist">
