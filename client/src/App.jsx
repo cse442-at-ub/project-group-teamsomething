66349d35593a2eba4768b0c16dd3bd0d549a0f6f
Erik Lichtenstein <eriklichtenstein2@gmail.com>
2023-11-16 00:28:40 -0500

    Revert "update routing to run on local host"
    This reverts commit 6d92e2a21044fa93b5e339f8f47e26edd468c4d9.
    
    nevermind was not running on localhost deleted too much

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,31 +1,150 @@
-import {
-  Route,
-  BrowserRouter as Router,
-  Routes,
-} from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
-import Public from "./pages/public/Public";
 import Login from "./pages/Login/Login";
 import SignUp from "./pages/SignUp/SignUp";
+import Public from "./pages/public/Public";
 
+import { ThemeProvider, createTheme } from "@mui/material";
 import Home from "./pages/Home/Home";
-import Partners from "./pages/Partners/Partners"
 import Matches from "./pages/Matches/Matches";
-import Message from "./pages/Message/Message";
+import Message_Blocked from "./pages/Message/Message_Blocked";
+import Partners from "./pages/Partners/Partners";
+import Profile from "./pages/User/Profile/Profile";
+import Description from "./pages/User/Description/Description";
+import PastContract from "./pages/User/PastContract/PastContract.jsx";
+import PartnerReview from "./pages/User/PartnerReview/PartnerReview.jsx";
+import Payment from "./pages/User/Payment/Payment.jsx";
+import ChangePassword from "./pages/User/ChangePassword/ChangePassword";
+import Message from "./pages/Message/Message.jsx";
+
+import { useContext, useEffect } from "react";
+
+import { useAuth } from "./hooks/auth-hook";
+import { AuthContext } from "./context/auth-context";
+import ContractPage from "./components/Contract/ContractPage.jsx";
+import Partner_Blocked from "./pages/Partners/Partner_Blocked.jsx";
 
 function App() {
+  const theme = createTheme({
+    components: {
+      MuiButton: {
+        styleOverrides: {
+          contained: {
+            borderRadius: "10px",
+            backgroundColor: "#EA4335",
+            color: "white",
+            "&:hover": {
+              backgroundColor: "#f5eeed",
+              color: "#EA4335",
+            },
+            boxShadow: "none",
+            cursor: "pointer",
+            textTransform: "none",
+          },
+          outlined: {
+            borderRadius: "10px",
+            borderColor: "black",
+            color: "black",
+            "&:hover": {
+              backgroundColor: "#EA4335",
+              color: "white",
+            },
+            boxShadow: "none",
+            cursor: "pointer",
+            textTransform: "none",
+          },
+        },
+      },
+      MuiTextField: {
+        styleOverrides: {
+          root: {
+            "& .MuiOutlinedInput-root": {
+              borderRadius: "20px",
+              backgroundColor: "#E8E9F4",
+              "& fieldset": {
+                borderColor: "#E8E9F4",
+              },
+              "&:hover": {
+                borderColor: "#E8E9F4",
+              },
+            },
+          },
+        },
+      },
+      MuiLinearProgress: {
+        styleOverrides: {
+          root: {
+            borderRadius: "20px",
+            backgroundColor: "#E8E9F4",
+            "& .MuiLinearProgress-bar": {
+              backgroundColor: "#EA4335",
+              borderRadius: "20px",
+            },
+            height: "12px",
+          },
+        },
+      },
+    },
+  });
+
+  const {
+    username,
+    fname,
+    lname,
+    partner,
+    login,
+    logout,
+    makePartner,
+    removePartner,
+  } = useAuth();
+
+  let protectedRoutes;
+  if (username) {
+    protectedRoutes = (
+      <>
+        <Route path={"/home"} element={<Home />}></Route>
+        <Route path={"/profile"} element={<Profile />} />
+        <Route path={"/description"} element={<Description />} />
+        <Route path={"/past-contracts"} element={<PastContract />}>
+          <Route path={"/past-contracts/:id"} element={<ContractPage />} />
+        </Route>
+        <Route path={"/change-password"} element={<ChangePassword />} />
+        <Route path={"/partner-reviews"} element={<PartnerReview />} />
+        <Route path={"/payment"} element={<Payment />} />
+        <Route path={"/message"} element={<Message />} />
+        <Route path={"/message-blocked"} element={<Message_Blocked />} />
+        <Route path={"/partners"} element={<Partners />} />
+        <Route path={"/partners-blocked"} element={<Partner_Blocked />} />
+        <Route path={"/matches"} element={<Matches />} />
+      </>
+    );
+  }
+
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Public />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/sign-up" element={<SignUp />} />
-        <Route path="/home" element={<Home />} />
-        <Route path="/Message" element={<Message />} />
-        <Route path="/partners"element={<Partners />} />
-        <Route path="/Matches" element={<Matches />} />
-      </Routes>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <AuthContext.Provider
+        value={{
+          username: username,
+          login: login,
+          logout: logout,
+          fname: fname,
+          lname: lname,
+          partner: partner,
+          makePartner: makePartner,
+          removePartner: removePartner,
+        }}
+      >
+        <Router basename="/CSE442-542/2023-Fall/cse-442x/dist">
+          <Routes>
+            <Route path={"/"} element={<Public />} />
+            <Route path={"/login"} element={<Login />} />
+            <Route path={"/sign-up"} element={<SignUp />} />
+            {/* protected routes below */}
+            {protectedRoutes}
+          </Routes>
+        </Router>
+      </AuthContext.Provider>
+    </ThemeProvider>
   );
 }
 
