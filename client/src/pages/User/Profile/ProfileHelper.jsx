36c1cc0530dc6d67a49556b40e27f45a4fe1044d
Erik Lichtenstein <eriklichtenstein2@gmail.com>
2023-12-06 21:15:14 -0500

    submit to edit username to be in line with rest of page

--- client/src/pages/User/Profile/ProfileHelper.jsx
+++ client/src/pages/User/Profile/ProfileHelper.jsx
@@ -230,7 +230,7 @@ export default function ProfileHelper() {
             </Stack>
             <Box flex={1}>
               <Button variant="contained" fullWidth type="submit" onClick={submitUsername}>
-                Submit
+                Edit Username
               </Button>
             </Box>
           </Stack>
